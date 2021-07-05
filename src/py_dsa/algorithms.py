0dc05f1b58efc582466e26d14ad256e2d6ad475c
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-07-05 23:02:49 +0530

    Added bubble and insertion sort

--- src/py_dsa/algorithms.py
+++ src/py_dsa/algorithms.py
@@ -1,4 +1,9 @@
 def linear_search(a, k):
+    """
+    Returns an integer
+
+    Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
+    """
     for i in range(len(a)):
         if a[i] == k:
             return i
@@ -6,6 +11,11 @@ def linear_search(a, k):
 
 
 def binary_search(a, k):
+    """
+    Returns an integer
+
+    Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
+    """
     first = 0
     last = len(a) - 1
     while first <= last:
@@ -17,3 +27,32 @@ def binary_search(a, k):
         elif a[mid] > k:
             last = mid - 1
     return -1
+
+
+def bubble_sort(a):
+    """
+    Returns None
+
+    Internally sorts the given list by using the bubble sort algorithm - repeatedly swapping the adjacent elements if they are in unsorted order
+    """
+    n = len(a)
+    for i in range(n-1):
+        for j in range(0, n - i - 1):
+            if a[j] >= a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
+
+def insertion_sort(a):
+    """
+    Returns None
+
+    Internally sorts the given list by using the insertion sort algorithm - list is virtually split into sorted and unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part. 
+    """
+    n = len(a)
+    for i in range(1, n):
+        temp = a[i]
+        j = i - 1
+        while(j >= 0 and temp < a[j]):
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = temp
--- test_py_dsa.py
+++ test_py_dsa.py
@@ -1,7 +1,21 @@
 from py_dsa.algorithms import *
 
+test_arr = [3, 1, 5, 4, 2]
+
+
 def test_py_dsa_algorithms_linear_search():
-    assert linear_search([1,2,3,4,5], 4) == 3
+    assert linear_search([1, 2, 3, 4, 5], 4) == 3
+
 
 def test_py_dsa_algorithms_binary_search():
-    assert binary_search([1,2,3,4,5], 3) == 2
+    assert binary_search([1, 2, 3, 4, 5], 3) == 2
+
+
+def test_py_dsa_algorithms_bubble_sort():
+    bubble_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_insertion_sort():
+    insertion_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
