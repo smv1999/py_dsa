f20288e46d6c5a12f966add9fa3a7dfe4fafb56c
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-07-08 20:26:33 +0530

    Done with Searching and Sorting Algorithms

--- src/py_dsa/algorithms.py
+++ src/py_dsa/algorithms.py
@@ -1,58 +1,226 @@
-def linear_search(a, k):
-    """
-    Returns an integer
+class Searching:
+    def linear_search(self, a, k):
+        """
+        Returns an integer
 
-    Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
-    """
-    for i in range(len(a)):
-        if a[i] == k:
-            return i
-    return -1
+        Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
+        """
+        for i in range(len(a)):
+            if a[i] == k:
+                return i
+        return -1
+
+    def binary_search(self, a, k):
+        """
+        Returns an integer
+
+        Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
+        """
+        first = 0
+        last = len(a) - 1
+        while first <= last:
+            mid = (first + last) // 2
+            if a[mid] == k:
+                return mid
+            elif a[mid] < k:
+                first = mid + 1
+            elif a[mid] > k:
+                last = mid - 1
+        return -1
+
+    def jump_search(self, a, k):
+        """
+        Returns an integer
+
+        Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
+        """
+        step = 0
+        while a[step] < k:
+            step += 1
+        i = step
+        while i < len(a):
+            if a[i] == k:
+                return i
+            i += step
+        return -1
 
 
-def binary_search(a, k):
-    """
-    Returns an integer
+class Sorting:
+    def bubble_sort(self, a):
+        """
+        Returns None
 
-    Index is returned, if the key element is found in the list. Otherwise, -1 is returned.
-    """
-    first = 0
-    last = len(a) - 1
-    while first <= last:
-        mid = (first + last) // 2
-        if a[mid] == k:
-            return mid
-        elif a[mid] < k:
-            first = mid + 1
-        elif a[mid] > k:
-            last = mid - 1
-    return -1
+        Internally sorts the given list by using the bubble sort algorithm - repeatedly swapping the adjacent elements if they are in unsorted order
+        """
+        n = len(a)
+        for i in range(n-1):
+            for j in range(0, n - i - 1):
+                if a[j] >= a[j+1]:
+                    a[j], a[j+1] = a[j+1], a[j]
 
+    def insertion_sort(self, a):
+        """
+        Returns None
 
-def bubble_sort(a):
-    """
-    Returns None
+        Internally sorts the given list by using the insertion sort algorithm - list is virtually split into sorted and unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part. 
+        """
+        n = len(a)
+        for i in range(1, n):
+            temp = a[i]
+            j = i - 1
+            while(j >= 0 and temp < a[j]):
+                a[j+1] = a[j]
+                j -= 1
+            a[j+1] = temp
 
-    Internally sorts the given list by using the bubble sort algorithm - repeatedly swapping the adjacent elements if they are in unsorted order
-    """
-    n = len(a)
-    for i in range(n-1):
-        for j in range(0, n - i - 1):
-            if a[j] >= a[j+1]:
-                a[j], a[j+1] = a[j+1], a[j]
+    def selection_sort(self, a):
+        """
+        Returns None
 
+        Internally sorts the given list by using the selection sort algorithm - repeatedly picking the smallest element from the unsorted part and placing it at the correct position in the sorted part.
+        """
+        n = len(a)
+        for i in range(n-1):
+            min_index = i
+            for j in range(i+1, n):
+                if a[j] < a[min_index]:
+                    min_index = j
+            if min_index != i:
+                a[i], a[min_index] = a[min_index], a[i]
 
-def insertion_sort(a):
-    """
-    Returns None
+    def quick_sort(self, a):
+        """
+        Returns None
 
-    Internally sorts the given list by using the insertion sort algorithm - list is virtually split into sorted and unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part. 
-    """
-    n = len(a)
-    for i in range(1, n):
-        temp = a[i]
-        j = i - 1
-        while(j >= 0 and temp < a[j]):
-            a[j+1] = a[j]
-            j -= 1
-        a[j+1] = temp
+        Internally sorts the given list by using the quick sort algorithm - the list is split into two parts. The left part is sorted, the right part is sorted recursively.
+        """
+        n = len(a)
+        self.__quick_sort_helper(a, 0, n-1)
+
+    def __quick_sort_helper(self, a, low, high):
+        if low < high:
+            pivot = self.__partition(a, low, high)
+            self.__quick_sort_helper(a, low, pivot-1)
+            self.__quick_sort_helper(a, pivot+1, high)
+
+    def __partition(self, a, low, high):
+        pivot = a[high]
+        i = low - 1
+        for j in range(low, high):
+            if a[j] <= pivot:
+                i += 1
+                a[i], a[j] = a[j], a[i]
+        a[i+1], a[high] = a[high], a[i+1]
+        return i+1
+
+    def merge_sort(self, a):
+        """
+        Returns None
+
+        Internally sorts the given list by using the merge sort algorithm - the list is split into two parts. The left part is sorted, the right part is sorted recursively.
+        """
+        if len(a) > 1:
+
+            mid = len(a)//2
+
+            L = a[:mid]
+
+            R = a[mid:]
+
+            self.merge_sort(L)
+
+            self.merge_sort(R)
+
+            i = j = k = 0
+
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    a[k] = L[i]
+                    i += 1
+                else:
+                    a[k] = R[j]
+                    j += 1
+                k += 1
+
+            while i < len(L):
+                a[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                a[k] = R[j]
+                j += 1
+                k += 1
+
+    def heap_sort(self, a):
+        """
+        Returns None
+
+        Internally sorts the given list by using the heap sort algorithm - the list is first converted into a heap. Then, the heap is repeatedly reduced to a sorted list.
+        """
+        n = len(a)
+
+        for i in range(n//2 - 1, -1, -1):
+            self.__heapify(a, n, i)
+
+        for i in range(n-1, 0, -1):
+            a[i], a[0] = a[0], a[i]
+            self.__heapify(a, i, 0)
+
+    def __heapify(self, a, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and a[l] > a[largest]:
+            largest = l
+        if r < n and a[r] > a[largest]:
+            largest = r
+        if largest != i:
+            a[i], a[largest] = a[largest], a[i]
+            self.__heapify(a, n, largest)
+
+    def radix_sort(self, a):
+        """
+        Returns None
+
+        Internally sorts the given list by using the radix sort algorithm - the list is first converted into a list of digits. Then, the list is repeatedly sorted by the digits.
+        """
+        max1 = max(a)
+
+        exp = 1
+        while max1 / exp > 0:
+            self.__counting_sort(a, exp)
+            exp *= 10
+
+    def __counting_sort(self, a, exp):
+        n = len(a)
+        b = [0] * n
+        count = [0] * 10
+        for i in range(0, n):
+            index = (a[i] // exp)
+            count[(index) % 10] += 1
+        for i in range(1, 10):
+            count[i] += count[i-1]
+        for i in range(n-1, -1, -1):
+            index = (a[i] // exp)
+            b[count[(index) % 10] - 1] = a[i]
+            count[(index) % 10] -= 1
+        for i in range(0, n):
+            a[i] = b[i]
+
+    def shell_sort(self, a):
+        """
+        Returns None
+
+        Internally sorts the given list by using the shell sort algorithm - the list is first converted into a list of sublists. Then, the list is repeatedly sorted by the sublists.
+        """
+        n = len(a)
+        h = 1
+        while h < n//3:
+            h = 3*h + 1
+        while h >= 1:
+            for i in range(h, n):
+                for j in range(i, h-1, -h):
+                    if a[j] < a[j-h]:
+                        a[j], a[j-h] = a[j-h], a[j]
+            h = h//3
--- test_py_dsa.py
+++ test_py_dsa.py
@@ -1,21 +1,64 @@
 from py_dsa.algorithms import *
 
-test_arr = [3, 1, 5, 4, 2]
+search_arr = Searching()
+sort_arr = Sorting()
 
 
 def test_py_dsa_algorithms_linear_search():
-    assert linear_search([1, 2, 3, 4, 5], 4) == 3
+    assert search_arr.linear_search([1, 2, 3, 4, 5], 4) == 3
 
 
 def test_py_dsa_algorithms_binary_search():
-    assert binary_search([1, 2, 3, 4, 5], 3) == 2
+    assert search_arr.binary_search([1, 2, 3, 4, 5], 3) == 2
+
+
+def test_py_dsa_algorithms_jump_search():
+    assert search_arr.jump_search([1, 2, 3, 4, 5], 5) == 4
 
 
 def test_py_dsa_algorithms_bubble_sort():
-    bubble_sort(test_arr)
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.bubble_sort(test_arr)
     assert test_arr == [1, 2, 3, 4, 5]
 
 
 def test_py_dsa_algorithms_insertion_sort():
-    insertion_sort(test_arr)
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.insertion_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_selection_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.selection_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_quick_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.quick_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_merge_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.merge_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_heap_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.heap_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_radix_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.radix_sort(test_arr)
+    assert test_arr == [1, 2, 3, 4, 5]
+
+
+def test_py_dsa_algorithms_shell_sort():
+    test_arr = [3, 1, 5, 4, 2]
+    sort_arr.shell_sort(test_arr)
     assert test_arr == [1, 2, 3, 4, 5]
